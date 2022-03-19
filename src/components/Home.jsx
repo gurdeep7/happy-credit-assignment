99f1fed09bcba77ba5cb8f64051df1a973490c06
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2022-03-19 07:30:27 +0530

    progress

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+export const Home = () =>{
+
+
+    return(<div className="inline-block w-full mt-12">
+    <div className="w-1/4 inline-block align-top bg-slate-300">
+<div className="text-3xl">Categories</div>
+    </div>
+    <div className="w-3/4 inline-block align-top bg-red-600 h-20">
+gdv cx
+    </div>
+    </div>)
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,22 +32,25 @@ Shop
 <div className="w-full">
 
 <div className="h-12 text-black hover:text-teal-500 text-left font-normal italic pl-20">Featured Stores</div>
-    <div className="h-12 font-normal text-black hover:text-teal-500 text-left text-3xl pl-20">Bed Bath And beyond</div>
-    <div className="h-12 font-normal text-black hover:text-teal-500 text-left text-3xl pl-20">Macy's</div>
-    <div className="h-12 font-normal text-black hover:text-teal-500 text-left text-3xl pl-20">Sephoria</div>
-    <div className="h-12 font-normal text-black hover:text-teal-500 text-left text-3xl pl-20">H&M</div>
+    <div className="h-12 font-normal text-black hover:text-teal-500 text-left text-2xl pl-20">Bed Bath And beyond</div>
+    <div className="h-12 font-normal text-black hover:text-teal-500 text-left text-2xl pl-20">Macy's</div>
+    <div className="h-12 font-normal text-black hover:text-teal-500 text-left text-2xl pl-20">Sephoria</div>
+    <div className="h-12 font-normal text-black hover:text-teal-500 text-left text-2xl pl-20">H&M</div>
     <div className="h-12 font-semibold flex text-teal-500 text-left text-xl pl-20">View all <AiOutlineArrowRight className="ml-2 mt-1" /></div>
 </div>
 </div>
 </button>
-<button className=" w-28 bg-slate-300 hover:text-teal-500 hover:underline">
+<button className=" w-28 font-bold bg-slate-300 hover:text-teal-500 hover:underline">
 How it works
 </button>
-<button className=" w-28 bg-slate-300 hover:text-teal-500 hover:underline">
+<button className=" w-28 font-bold bg-slate-300 hover:text-teal-500 hover:underline">
 play in 4
 </button>
-<button className=" w-36 bg-slate-300 hover:text-teal-500 hover:underline">
+<button className=" w-36 font-bold bg-slate-300 hover:text-teal-500 hover:underline">
 The Shopping app
+</button>
+<button className=" w-28 font-bold bg-slate-300 hover:text-teal-500 hover:underline">
+Help
 </button>
 </div>
 <div className="fixed right-0 m-2 bg-slate-200  ">
@@ -57,9 +60,14 @@ The Shopping app
 <button className="border-2 hidden m-3 lg:inline-block rounded-2xl w-28 bg-black text-white hover:bg-white hover:text-black pb-1 border-black">
     Get the app
 </button>
-<button className="inline-block relative p-3 lg:hidden pb-1">
+<button className="inline-block relative p-3 lg:hidden pb-1 hamburger">
 <BiMenu className="text-4xl block" />
-<div className="absolute">
+<div className="absolute hidden right-0 mt-3 w-96 hamburger-content shadow-2xl">
+<div className="h-20 font-bold text-black hover:text-teal-500 mt-3 text-3xl ">Shop</div>
+<div className="h-20 font-bold text-black hover:text-teal-500  text-3xl ">How it works</div>
+<div className="h-20 font-bold text-black hover:text-teal-500  text-3xl ">Play in 4</div>
+<div className="h-20 font-bold text-black hover:text-teal-500  text-3xl ">The shopping help</div>
+<div className="h-20 font-bold text-black hover:text-teal-500  text-3xl ">Help</div>
 
 </div>
 </button>
